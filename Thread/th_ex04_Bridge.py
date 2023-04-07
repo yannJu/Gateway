a226d3a56485f1a1d3afa38dcec238fb7a5eac45
yannJu <yann_ju@naver.com>
2023-04-07 17:33:46 +0900

    스레드 동기화 Lock 이용하여 해결

--- Thread/th_ex04_Bridge.py
+++ Thread/th_ex04_Bridge.py
@@ -1,14 +1,21 @@
+from threading import Lock
+
 class Bridge:
     def __init__(self):
         self.counter = 0
         self.name = "O"
         self.address = "X"
         
+        # 동기화 해결
+        self.lock = Lock()
+        
     def across(self, name, address):
+        self.lock.acquire()
         self.counter += 1
         self.name = name
         self.address = address
         self.check()
+        self.lock.release()
         
     def toString(self):
         return "이름 : {}, 출신 : {},  도전횟수 : {}".format(self.name, self.address, self.counter)
