70b045daa2a5b4e106037e556145692094f38bd9
yannJu <yann_ju@naver.com>
2023-04-07 16:44:20 +0900

    th_ex03_Thread 를 상속받아 진행

---
+++ Thread/th_ex03_inherit_thread.py
@@ -0,0 +1,18 @@
+import threading, requests, time
+
+class HtmlGetter(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self) #super().__init__() 시 self 안붙임
+        self.url = url
+        
+    def run(self):
+        resp = requests.get(self.url)
+        time.sleep(1)
+        
+        print(self.url, len(resp.text), "\n", resp.text)
+        
+url = "https://www.google.com"
+thread = HtmlGetter(url)
+thread.start()
+# thread.run()
+print("END")
