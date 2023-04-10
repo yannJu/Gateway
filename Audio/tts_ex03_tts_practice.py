fff8286dca825d95b726f4568eacec1f86175c78
yannJu <yann_ju@naver.com>
2023-04-10 15:24:21 +0900

    mp3 파일 출력 코드 작성

--- Audio/tts_ex03_tts_practice.py
+++ Audio/tts_ex03_tts_practice.py
@@ -9,9 +9,10 @@ while True:
     
     f = BytesIO()
     tts = gTTS(text = text, lang = 'ko')
-    tts.save2(f)
+    tts.save(f)
     
     f.seek(0) #write 후 포인터가 이동하므로 읽기 위해서는 seek 사용
     
     song = AudioSegment.from_mp3(f) #BytesIO 로 읽어오기
-    play(song)
+    song.export('convert_yanjo.mp3', format='mp3', codec='libmp3lame')
+    # play(song)g
