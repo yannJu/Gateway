58407c2492a1c4c618ddc920d94e49241fddf833
yannJu <yann_ju@naver.com>
2023-05-03 17:20:02 +0900

    NodeMCU_MQTT_v0.3 다시 생성

--- Server/NodeMCU_MQTT_v0.3/hotkey.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-doskey makemigrations = python manage.py makemigrations
-doskey migrate = python manage.py migrate
-doskey runserver = python manage.py runserver 0.0.0.0:8000
--- Server/NodeMCU_MQTT_v0.3/kakao/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('login/', KakaoLoginView.as_view()),
-    path('oauth', KakaoAuthView.as_view(), name="oatuh"),
-    path('talk', KakaoTalkView.as_view(), name = 'talk'),
-]
